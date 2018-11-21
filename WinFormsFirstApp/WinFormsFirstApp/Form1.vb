5cfd8b068d36d626e45a838ae80ef5540a046b24
Muhammad Hafidz Misrudin <hafidz@ksc.kaf.net>
2018-11-21 15:55:41 +0800

    built simple desktop app with Windows Form in vb.net

---
+++ WinFormsFirstApp/WinFormsFirstApp/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim enteredValue As String
+        enteredValue = TextBox1.Text
+
+        enteredValue = "Added: " + enteredValue
+        Label2.Text = enteredValue
+    End Sub
+End Class
