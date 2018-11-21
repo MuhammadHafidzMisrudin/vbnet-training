e5e63a3950794fcb13f255b31ff0477e51deefaf
Hafidz <halfpheez92@gmail.com>
2018-11-21 17:42:29 +0800

    built simple desktop application with WPF in vb.net.

---
+++ WpfFirstApp/WpfFirstApp/MainWindow.xaml.vb
@@ -0,0 +1,9 @@
+﻿Class MainWindow
+    Private Sub ButtonObject1_Click(sender As Object, e As RoutedEventArgs) Handles ButtonObject1.Click
+        Dim enteredValue As String
+        enteredValue = TextBoxObject1.Text
+
+        enteredValue = "Added: " + enteredValue
+        LabelObject1.Content = enteredValue
+    End Sub
+End Class
