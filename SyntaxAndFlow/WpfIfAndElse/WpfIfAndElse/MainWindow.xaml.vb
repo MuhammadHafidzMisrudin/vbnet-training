e2d79a66205896e8ffe3ec626a983f37eb0ab6a9
Hafidz <halfpheez92@gmail.com>
2018-11-22 12:56:00 +0800

    designed wfp form and implemented functionalities of output text.

--- SyntaxAndFlow/WpfIfAndElse/WpfIfAndElse/MainWindow.xaml.vb
+++ SyntaxAndFlow/WpfIfAndElse/WpfIfAndElse/MainWindow.xaml.vb
@@ -1,3 +1,19 @@
 ﻿Class MainWindow
 
+    Sub OutputHandler(sender As Object, e As RoutedEventArgs)
+        Dim valueOne As String = inputTextOne.Text
+        Dim valueTwo As String = inputTextTwo.Text
+
+        OutputText(valueOne)
+        OutputText(valueTwo)
+
+    End Sub
+
+    Sub OutputText(Value As String)
+        textOutput.Text += Value + vbCrLf
+    End Sub
+
+    Sub OutputClear(sender As Object, e As RoutedEventArgs)
+        textOutput.Text = ""
+    End Sub
 End Class
