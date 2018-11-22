e4137fb9c22566d8ae74d7c4a2de32998b033f1d
Hafidz <halfpheez92@gmail.com>
2018-11-22 10:20:01 +0800

    implemented WPF code runner method form.

---
+++ WpfCodeRunner/CodeRunner/MainWindow.xaml.vb
@@ -0,0 +1,19 @@
+﻿Class MainWindow 
+
+    Sub RunCode(sender As Object, e As RoutedEventArgs)
+        'Add code here
+        Dim namedString As String = ""
+        namedString = txtInput.Text
+        namedString = "Added: " & namedString
+        Output(namedString)
+    End Sub
+
+    Sub Output(Value As String)
+        txtOutput.Text += Value + vbCrLf
+    End Sub
+
+    Sub ClearOutput(sender As Object, e As RoutedEventArgs)
+        txtOutput.Text = ""
+    End Sub
+
+End Class
