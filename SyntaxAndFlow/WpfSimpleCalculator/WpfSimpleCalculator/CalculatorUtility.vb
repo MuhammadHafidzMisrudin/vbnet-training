7bfc909ebbe9b60fc9f8a5965bd4165d63d84786
Hafidz <halfpheez92@gmail.com>
2018-11-30 14:46:38 +0800

    added comments.

--- SyntaxAndFlow/WpfSimpleCalculator/WpfSimpleCalculator/CalculatorUtility.vb
+++ SyntaxAndFlow/WpfSimpleCalculator/WpfSimpleCalculator/CalculatorUtility.vb
@@ -23,24 +23,28 @@
         End Set
     End Property
 
+    'Create a Constructor'
     Sub New(dbl1 As Double, dbl2 As Double)
         value1 = dbl1
         value2 = dbl2
     End Sub
 
-
+    'Addition method'
     Public Function AddValues() As Double
         Return value1 + value2
     End Function
 
+    'Substraction method'
     Public Function SubtractValues() As Double
         Return value1 - value2
     End Function
 
+    'Multiplication method'
     Public Function MultiplyValues() As Double
         Return value1 * value2
     End Function
 
+    'Division method'
     Public Function DivideValues() As Double
         Return value1 / value2
     End Function
