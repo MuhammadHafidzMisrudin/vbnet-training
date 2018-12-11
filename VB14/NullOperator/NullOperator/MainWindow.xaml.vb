869940620a81b97a081ba326290be9ed06a3f345
Hafidz <halfpheez92@gmail.com>
2018-12-11 11:42:02 +0800

    implemented checking for null values with null-propagating operator

---
+++ VB14/NullOperator/NullOperator/MainWindow.xaml.vb
@@ -0,0 +1,40 @@
+﻿Class MainWindow
+
+    Sub RunCode(sender As Object, e As RoutedEventArgs)
+        'Add code here
+
+        Dim StringVar As String = "Hello Inma"
+
+        'Checking if string variable is null'
+        If StringVar IsNot Nothing Then
+            Dim StrLength As Integer = StringVar.Length
+            Output("String length = " + StrLength.ToString())
+        Else
+            Output("The string variable is empty")
+        End If
+
+        Output("")
+
+        'Use and implement checking for null values with null-propagating operator (?.)'
+        Dim StringVariable As String = Nothing
+        Dim StrVarLength As Integer? = StringVariable?.Length
+        Output("String length for new variable = " + StrVarLength.ToString())
+
+        Output("")
+
+        Output("Length (explicitly use if clause) = " + If(StrVarLength Is Nothing, "0", StrVarLength.ToString()))
+
+        Output("")
+        Output("All done!")
+
+    End Sub
+
+    Sub Output(Value As String)
+        txtOutput.Text += Value + vbCrLf
+    End Sub
+
+    Sub ClearOutput(sender As Object, e As RoutedEventArgs)
+        txtOutput.Text = ""
+    End Sub
+
+End Class
