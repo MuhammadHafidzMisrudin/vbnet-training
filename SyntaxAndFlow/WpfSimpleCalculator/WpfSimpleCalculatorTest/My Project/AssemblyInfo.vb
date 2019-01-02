d29bf27199a49a43bdd7b2b448a1ab9f648a2fc6
Hafidz <halfpheez92@gmail.com>
2019-01-02 17:59:44 +0800

    created and added a unit test project and unit test.

---
+++ SyntaxAndFlow/WpfSimpleCalculator/WpfSimpleCalculatorTest/My Project/AssemblyInfo.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("WpfSimpleCalculatorTest")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HP Inc.")>
+<Assembly: AssemblyProduct("WpfSimpleCalculatorTest")>
+<Assembly: AssemblyCopyright("Copyright © HP Inc. 2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+<Assembly: Guid("830dae91-572e-4551-9c15-6cc6e54e4bf2")>
+
+' <Assembly: AssemblyVersion("1.0.*")> 
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ SyntaxAndFlow/WpfSimpleCalculator/WpfSimpleCalculatorTest/UnitTest1.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports WpfSimpleCalculator
+
+<TestClass()> Public Class UnitTest1
+
+    <TestMethod()> Public Sub TestMethod1()
+        Dim test As New CalculatorUtility(1.0, 2.0)
+        test.AddValues()
+        Assert.AreEqual(3.0, test.result)
+    End Sub
+
+End Class
